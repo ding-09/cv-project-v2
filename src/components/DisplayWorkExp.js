5bea87a1d9d0128c667096c7952f72db4c7c6559
tiffany <yang.ding@hotmail.com>
2020-11-08 23:03:40 -0800

    added save work and delete work buttons and functions

---
+++ src/components/DisplayWorkExp.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function FormatDesc(props) {
+  const { desc } = props;
+  return (
+    <ul className="work-desc-list">
+      {desc.split('\n').map((bulletPoint) => {
+        return <li>{bulletPoint}</li>;
+      })}
+    </ul>
+  );
+}
+
+function DeleteBtn(props) {
+  const { id } = props;
+
+  return (
+    <div className="delete-btn-container">
+      <button
+        className="delete-btn"
+        onClick={props.deleteHist.bind(this, id)}
+      >
+        X
+      </button>
+    </div>
+  );
+}
+
+function DisplayWorkExp(props) {
+  const { workHist } = props;
+
+  return (
+    <div className="render-info">
+      <h4 className="main-title">{workHist.jobTitle}</h4>
+      <p className="date-info">{workHist.dateWorked}</p>
+      <p className="sub-info">{workHist.employer}</p>
+      <p className="sub-info">{workHist.location}</p>
+      <div className="formatted-list">
+        <FormatDesc desc={workHist.description} />
+      </div>
+      <DeleteBtn id={workHist.id} deleteHist={props.deleteHist} />
+    </div>
+  );
+}
+
+export default DisplayWorkExp;
--- src/components/Work.js
+++ src/components/Work.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import SectionHeading from '../layout/SectionHeading';
 import OpenModalBtn from './OpenModalBtn';
 import WorkExpForm from './WorkExpForm';
+import DisplayWorkExp from './DisplayWorkExp';
 
 function Work() {
   const [displayForm, setDisplay] = useState(false);
@@ -12,15 +13,32 @@ function Work() {
   };
 
   const saveWork = (workInfo) => {
-      const newWorkExp = [...workExp, workInfo];
-      addWorkExp(newWorkExp);
-      handleDisplay();
+    const newWorkExp = [...workExp, workInfo];
+    addWorkExp(newWorkExp);
+    handleDisplay();
   };
-    
+
+  const deleteHist = (id) => {
+    const newWorkExp = [...workExp];
+    newWorkExp.splice(id, 1);
+    addWorkExp(newWorkExp);
+  };
+
   return (
     <>
       <SectionHeading title="Work Experience" />
-      <WorkExpForm displayForm={displayForm} handleDisplay={handleDisplay} saveWork={saveWork}/>
+      <WorkExpForm
+        displayForm={displayForm}
+        handleDisplay={handleDisplay}
+        saveWork={saveWork}
+      />
+      {workExp.map((workHist, index) => (
+        <DisplayWorkExp
+          workHist={workHist}
+          key={index}
+          deleteHist={deleteHist}
+        />
+      ))}
       <OpenModalBtn title="Work Experience" handleDisplay={handleDisplay} />
     </>
   );
